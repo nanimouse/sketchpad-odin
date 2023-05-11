ac8323d9ff94a8c83f40332e6fa3e0cda6c5fe91
nanimouse <0120codes@gmail.com>
2023-05-11 15:34:18 -0400

    finished making and centering the grid

--- script.js
+++ script.js
@@ -3,8 +3,21 @@
 let body = document.querySelector("body");
 let container = document.querySelector("#container");
 
-for(i=0; i<16; i++){
-    let div = document.createElement("div");
-    div.setAttribute('class', 'grid');
-    container.appendChild(div);
+// for(i=0; i<16; i++){
+//     let div = document.createElement("div");
+//     div.setAttribute('class', 'grid');
+//     container.appendChild(div);
+// }
+    //creates divs in a single row
+
+for(rowNum=0; rowNum<16; rowNum++){
+    let row = document.createElement("div");
+    row.setAttribute('class', 'row');
+    container.appendChild(row);
+    for(colNum=0; colNum<16; colNum++){
+        let col = document.createElement("div");
+        col.setAttribute('class', 'col');
+        row.appendChild(col);
+    }
 }
+
